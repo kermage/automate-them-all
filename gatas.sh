3ac3a029527e0b7b666a816bbe6c18395f71fff1
kermage <genealyson.torcende@gmail.com>
2017-02-13 09:01:21 +0800

    resaved with EditorConfig and changed interpreter

--- gatas.sh
+++ gatas.sh
@@ -1,4 +1,4 @@
-#!/system/bin/sh
+#!/bin/sh
 
 #      ,#####    ###    ,#############
 #     ###  `##  ## ##   ###      ##
@@ -31,73 +31,73 @@
 REAL_DIRECTORY=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 
 function isFunction() {
-    declare -F $1 &> /dev/null
-    return $?
+	declare -F $1 &> /dev/null
+	return $?
 }
 
 function init() {
-    cp $REAL_DIRECTORY/gatas.cfg gatas.cfg
-    
-    read -p "Server Host: " SV_HOST
-    read -p "Server User: " SV_USER
-    read -p "Server Port: " SV_PORT
-    read -p "Remote Path: " RM_PATH
-    read -p "Database User: " DB_USER
-    read -p "Database Pass: " DB_PASS
-    read -p "Database Name: " DB_NAME
-    
-    sed -i '/^SV_HOST/s/=.*/='$SV_HOST'/' gatas.cfg
-    sed -i '/^SV_USER/s/=.*/='$SV_USER'/' gatas.cfg
-    sed -i '/^SV_PORT/s/=.*/='$SV_PORT'/' gatas.cfg
-    sed -i '/^RM_PATH/s/=.*/='${RM_PATH//\//\\/}'/' gatas.cfg
-    sed -i '/^DB_USER/s/=.*/='$DB_USER'/' gatas.cfg
-    sed -i '/^DB_PASS/s/=.*/='$DB_PASS'/' gatas.cfg
-    sed -i '/^DB_NAME/s/=.*/='$DB_NAME'/' gatas.cfg
+	cp $REAL_DIRECTORY/gatas.cfg gatas.cfg
+
+	read -p "Server Host: " SV_HOST
+	read -p "Server User: " SV_USER
+	read -p "Server Port: " SV_PORT
+	read -p "Remote Path: " RM_PATH
+	read -p "Database User: " DB_USER
+	read -p "Database Pass: " DB_PASS
+	read -p "Database Name: " DB_NAME
+
+	sed -i '/^SV_HOST/s/=.*/='$SV_HOST'/' gatas.cfg
+	sed -i '/^SV_USER/s/=.*/='$SV_USER'/' gatas.cfg
+	sed -i '/^SV_PORT/s/=.*/='$SV_PORT'/' gatas.cfg
+	sed -i '/^RM_PATH/s/=.*/='${RM_PATH//\//\\/}'/' gatas.cfg
+	sed -i '/^DB_USER/s/=.*/='$DB_USER'/' gatas.cfg
+	sed -i '/^DB_PASS/s/=.*/='$DB_PASS'/' gatas.cfg
+	sed -i '/^DB_NAME/s/=.*/='$DB_NAME'/' gatas.cfg
 }
 
 function test() {
-    SV_HOST=`grep '^SV_HOST' gatas.cfg | sed 's/.*=//'`
-    SV_USER=`grep '^SV_USER' gatas.cfg | sed 's/.*=//'`
-    SV_PORT=`grep '^SV_PORT' gatas.cfg | sed 's/.*=//'`
-    DB_USER=`grep '^DB_USER' gatas.cfg | sed 's/.*=//'`
-    DB_PASS=`grep '^DB_PASS' gatas.cfg | sed 's/.*=//'`
-    DB_NAME=`grep '^DB_NAME' gatas.cfg | sed 's/.*=//'`
-    
-    echo -n "Testing Local Database . . . "
-    mysql --user=$DB_USER --password=$DB_PASS $DB_NAME -e "exit" > /dev/null 2>&1
-    if [ "$?" -eq 0 ]; then
-        echo "PASSED!"
-    else
-        echo "FAILED!"
-    fi
-    
-    echo -n "Testing SSH Connection . . . "
-    ssh $SV_USER@$SV_HOST -p$SV_PORT "exit" > /dev/null 2>&1
-    if [ "$?" -eq 0 ]; then
-        echo "PASSED!"
-    else
-        echo "FAILED!"
-    fi
-    
-    echo -n "Testing Remote Database . . . "
-    ssh $SV_USER@$SV_HOST -p$SV_PORT "mysql --user=$DB_USER --password=$DB_PASS $DB_NAME -e 'exit'" > /dev/null 2>&1
-    if [ "$?" -eq 0 ]; then
-        echo 'PASSED!'
-    else
-        echo 'FAILED!'
-    fi
+	SV_HOST=`grep '^SV_HOST' gatas.cfg | sed 's/.*=//'`
+	SV_USER=`grep '^SV_USER' gatas.cfg | sed 's/.*=//'`
+	SV_PORT=`grep '^SV_PORT' gatas.cfg | sed 's/.*=//'`
+	DB_USER=`grep '^DB_USER' gatas.cfg | sed 's/.*=//'`
+	DB_PASS=`grep '^DB_PASS' gatas.cfg | sed 's/.*=//'`
+	DB_NAME=`grep '^DB_NAME' gatas.cfg | sed 's/.*=//'`
+
+	echo -n "Testing Local Database . . . "
+	mysql --user=$DB_USER --password=$DB_PASS $DB_NAME -e "exit" > /dev/null 2>&1
+	if [ "$?" -eq 0 ]; then
+		echo "PASSED!"
+	else
+		echo "FAILED!"
+	fi
+
+	echo -n "Testing SSH Connection . . . "
+	ssh $SV_USER@$SV_HOST -p$SV_PORT "exit" > /dev/null 2>&1
+	if [ "$?" -eq 0 ]; then
+		echo "PASSED!"
+	else
+		echo "FAILED!"
+	fi
+
+	echo -n "Testing Remote Database . . . "
+	ssh $SV_USER@$SV_HOST -p$SV_PORT "mysql --user=$DB_USER --password=$DB_PASS $DB_NAME -e 'exit'" > /dev/null 2>&1
+	if [ "$?" -eq 0 ]; then
+		echo 'PASSED!'
+	else
+		echo 'FAILED!'
+	fi
 }
 
 if [ $# -gt 0 ]; then
-    COMMAND=$1
-    
-    if ( isFunction "$COMMAND" ); then
-        "$COMMAND"
-    else
-        COMMAND_FILE=$( ls ${REAL_DIRECTORY}/${COMMAND} 2> /dev/null | head -1 )
-        
-        if [ -r "$COMMAND_FILE" ]; then
-            sh "$COMMAND_FILE" "${@: 2}"
-        fi
-    fi
+	COMMAND=$1
+
+	if ( isFunction "$COMMAND" ); then
+		"$COMMAND"
+	else
+		COMMAND_FILE=$( ls ${REAL_DIRECTORY}/${COMMAND} 2> /dev/null | head -1 )
+
+		if [ -r "$COMMAND_FILE" ]; then
+			sh "$COMMAND_FILE" "${@: 2}"
+		fi
+	fi
 fi
